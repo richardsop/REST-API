0e1b042f51b286cd0982d451550e6bbc54003e3a
richardsop <richardsop2@gmail.com>
2016-01-05 21:14:25 +0100

    commit

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 # import the Flask class from the flask module
 from flask import Flask, render_template, redirect, \
-    url_for, request, session, flash, g, jsonify
-from functools import wraps
+    url_for, request, session, flash, g, jsonify,json
+#from functools import wraps
 import sqlite3
 
 # create the application object
@@ -17,7 +17,7 @@ app.database = 'sample.db'
 def home():
     # return a string
     g.db = connect_db()
-    cur = g.db.execute('select * from NationalNames LIMIT 5')
+    cur = g.db.execute('select * from NationalNames LIMIT 5') # limit so as to enhance speed
     NationalNames = [dict(Id=row[0], Name=row[1], Year=row[2], Gender=row[3], Count = row[4]) for row in cur.fetchall()]
     g.db.close()
     return render_template('index.html', NationalNames=NationalNames)  # render a template
@@ -38,7 +38,6 @@ def  babyname():
     g.db = connect_db()
     cur = g.db.execute('select * from NationalNames LIMIT 20') # limit so as to enhance speed
     NationalNames = [dict(Id=row[0], Name=row[1], Year=row[2], Gender=row[3], Count = row[4]) for row in cur.fetchall()]
-    #namebaby = [NationalName for NationalName in NationalNames if NationalName ['Name'] == name]
     return jsonify({'Baby names are ' : NationalNames})
 
 #get all entries for a specific baby name
@@ -49,8 +48,31 @@ def  returnOne(name):
     cur = g.db.execute('select * from NationalNames LIMIT 100') # limit so as to enhance speed
     NationalNames = [dict(Id=row[0], Name=row[1], Year=row[2], Gender=row[3], Count = row[4]) for row in cur.fetchall()]
     namebaby = [NationalName for NationalName in NationalNames if NationalName ['Name'] == name]
-    return jsonify({'Baby name detail is' : namebaby[0]})
-   
+    return jsonify({'Baby name details are' : namebaby[0]})
+
+# list of all unique baby names
+@app.route('/unique', methods = ['GET'])
+def unique():
+    # return a string
+    g.db = connect_db()
+    cur2 = g.db.execute('select * from NationalNames group by Name LIMIT 10')
+    #cur = g.db.execute('select avg(Count) from NationalNames LIMIT 10')
+    NationalNames = [dict(Name=row[1], Year=row[2], Count = row[4]) for row in cur2.fetchall()]
+    #NationalNames2 = [dict(Count = row[2]) for row in cur.fetchall()]
+    return jsonify({'Unique names are' : NationalNames})
+
+#add entry to database 
+@app.route('/add', methods=['POST'])
+def add_entry():
+    cur = g.db.cursor()
+    g.db.execute('insert into NationalNames (Gender, Year, Count) values (?, ?, ?)',
+                 [request.json['gender'], request.json['year'], request.json['count']])
+    g.db.commit()
+    id = cur.lastrowid
+    cur.close()
+    flash('New entry was successfully posted')
+    return "id"
+
 
 @app.route('/welcome')
 def welcome():
